4bea8fa5c56a5d0272bdb0754935707674c2b0c6
Andrew Dang <{ID}+{username}@users.noreply.github.com>
2022-06-27 09:56:54 -0400

    removed excess imports

--- rpghub/src/main/java/com/revature/scramble/App.java
+++ rpghub/src/main/java/com/revature/scramble/App.java
@@ -3,12 +3,9 @@ package com.revature.scramble;
 import com.revature.scramble.controller.AccountController;
 import com.revature.scramble.controller.HomeController;
 import com.revature.scramble.controller.SessionController;
-import com.revature.scramble.models.Session;
 
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
-import com.revature.scramble.repository.LoginInfoDao;
-import com.revature.scramble.repository.entities.LoginInfo;
 
 public class App 
 {
--- rpghub/src/main/java/com/revature/scramble/repository/UserInfoDao.java
+++ rpghub/src/main/java/com/revature/scramble/repository/UserInfoDao.java
@@ -5,8 +5,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.revature.scramble.repository.entities.UserInfo;
 import com.revature.scramble.repository.interfaces.UserInfoDaoInterface;

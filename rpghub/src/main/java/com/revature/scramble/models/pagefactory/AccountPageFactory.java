8362b4175b7d928a4cc434fc68c1e802bf962bde
Anthony Baker <tylerb102598@gmail.com>
2022-07-02 20:10:15 -0400

    worked on test for account page

--- rpghub/src/main/java/com/revature/scramble/models/pagefactory/AccountPageFactory.java
+++ rpghub/src/main/java/com/revature/scramble/models/pagefactory/AccountPageFactory.java
@@ -1,12 +1,14 @@
 package com.revature.scramble.models.pagefactory;
 
 import java.util.List;
+import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class AccountPageFactory {
 
@@ -45,8 +47,9 @@ public class AccountPageFactory {
     @FindBy(id = "create-max-size")
     WebElement max_size_field;
 
-    @FindBy(className = "listing-submit")
+    @FindBy(id = "listing-submit")
     WebElement listing_submit_button;
+
 
     public AccountPageFactory(WebDriver webDriver){
         this.webdriver = webDriver;
@@ -63,6 +66,53 @@ public class AccountPageFactory {
     public void click_select_listing_button(){
         get_select_listing_element().click();
     }
+
+    public void click_filter_button() {
+        filter_button.click();
+    }
+
+    public void select_category(){
+        Select category_dropdown = new Select(filter_category_dropdown);
+        List<WebElement> categories = category_dropdown.getOptions();
+        
+        int random_category = (int)Math.random()*categories.size();
+        category_dropdown.selectByIndex(random_category);
+    }
+
+    public void input_specific_filter(String specific_filter){
+        specific_filter_input.sendKeys(specific_filter);
+    }
+
+    public void apply_filter(){
+        filter_button.click();
+    }
+
+    public void click_create_listing_button() {
+        create_listing_button.click();
+    }
+
+    public void enter_listing_name(String list_name){
+        list_name_field.sendKeys(list_name);
+    }
+
+    public void input_dungeon(String dungeon_name) {
+        dungeon_name_field.sendKeys(dungeon_name);
+    }
+
+    public void list_max_party_size() {
+        Random rand = new Random();
+        int max_size = 8;
+        int min_size = 2;
+        int rand_max_size = rand.nextInt(max_size);
+        max_size+=min_size;
+        max_size_field.sendKeys(String.valueOf(rand_max_size));
+    }
+
+    public void click_listing_submit_button(){
+        listing_submit_button.click();
+    }
+
+
 
 
 
--- rpghub/src/test/java/com/revature/scramble/StepDefinitions/AccountDemoSteps.java
+++ rpghub/src/test/java/com/revature/scramble/StepDefinitions/AccountDemoSteps.java
@@ -35,6 +35,58 @@ public class AccountDemoSteps {
         accountPageFactory = new AccountPageFactory(driver);
     }
 
+    @Given("a user is logged in and on the Account page")
+    public void a_user_is_logged_in_and_on_the_account_page() {
+        loginPageFactory.login("user1","pass");
+        Assert.assertEquals(driver.getTitle(), "Account");
+    }
+    @When("user clicks Filter Listing button")
+    public void user_clicks_filter_listing_button() {
+        accountPageFactory.click_filter_button();
+    }
+    @When("user chooses a category to filter by")
+    public void user_chooses_a_category_to_filter_by() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+        accountPageFactory.select_category();
+        }
+    @When("user inputs a specific filter")
+    public void user_inputs_a_specific_filter() {
+        accountPageFactory.input_specific_filter("any string less than 50 chars");
+    }
+    @When("user clicks Filter button")
+    public void user_clicks_filter_button() {
+        accountPageFactory.apply_filter();
+    }
+
+    @When("user clicks Create Listing button")
+    public void user_clicks_create_listing_button() {
+        accountPageFactory.click_create_listing_button();
+    }
+    @When("user inputs a list name")
+    public void user_inputs_a_list_name() {
+        accountPageFactory.enter_listing_name("new list name less than 50 char");
+    }
+    @When("user inputs a dungeon name")
+    public void user_inputs_a_dungeon_name() {
+        accountPageFactory.input_dungeon("dungeon name");
+    }
+    @When("user inputs max party size")
+    public void user_inputs_max_party_size() {
+        accountPageFactory.list_max_party_size();
+    }
+    @When("user clicks Submit button")
+    public void user_clicks_submit_button() {
+        accountPageFactory.click_listing_submit_button();
+    }
+    @Then("the listing should be shown")
+    public void the_listing_should_be_shown() {
+        Assert.assertEquals(driver.getTitle(), "Account");
+    }
+    @Then("there should be a new listing")
+    public void there_should_be_a_new_listing() {
+        Assert.assertEquals(driver.getTitle(), "Account");
+    }   
+
     @After
     public void teardown(){
         if(!driver.getTitle().equals("Homepage")){
